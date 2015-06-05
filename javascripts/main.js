c1f3bf3a632fc916880cafc8b1ac45e75c6dd9db
Zhenjie Ruan <zhenjier@bu.edu>
2015-06-05 20:31:23 +0800

    now added images and the image for network aware container distribution system now linked to the demo vedio on youtube

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,9 @@
-console.log('This would be the main JS file.');
+$(document).ready(function(){
+	$("a.video_pop_up").fancybox({
+	'speedIn'		:	600, 
+	'speedOut'		:	200, 
+	'width	'		:	800, 
+	'height'		:	400, 
+	'type'			:	'iframe'
+	});
+});
